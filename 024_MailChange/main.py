5076b4d24c4c68931d502fd7123ef9b35e80eb2d
KrzRak <krzysztofrak93@gmail.com>
2022-08-16 18:49:34 +0200

    Folder rename

--- 024_MailChange/main.py
+++ 024_MailChange/main.py
@@ -7,15 +7,15 @@
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-with open("Input/Names/invited_names.txt", mode="r") as invited_names:
+with open("./024_MailChange/Input/Names/invited_names.txt", mode="r") as invited_names:
     names = invited_names.readlines()
 
 for names_id in range(0, len(names)):
     names[names_id] = names[names_id].strip()
 
-    with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
+    with open("./024_MailChange/Input/Letters/starting_letter.txt", mode="r") as starting_letter:
         starting_letter_read = starting_letter.read()
         starting_letter_read = starting_letter_read.replace("[name]", names[names_id])
 
-    with open(f"Output/ReadyToSend/{names[names_id]}_letter.txt", mode="w") as name_letter:
+    with open(f"./024_MailChange/Output/ReadyToSend/{names[names_id]}_letter.txt", mode="w") as name_letter:
         name_letter.write(starting_letter_read)
