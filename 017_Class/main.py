c7b2faa00eb07baad9a82bb9f89eb3653a8bede9
KrzRak <krzysztofrak93@gmail.com>
2022-08-16 16:25:59 +0200

    Folder rename; Add comments; Functionality check

--- 017_Class/main.py
+++ 017_Class/main.py
@@ -1,3 +1,7 @@
+# How to declare and use class in phyton
+
+# Declaration
+
 class User:
 
     def __init__(self, id, username):
@@ -10,13 +14,16 @@ class User:
         user.followers += 1
         self.following += 1
 
-
+# Declaration for users using one class
 user_1 = User("001", "angela")
 user_2 = User("002", "jack")
 
+# Using function from class
 user_1.follow(user_2)
 user_1.follow(user_2)
 user_1.follow(user_2)
+
+# Output
 print(user_1.followers)
 print(user_1.following)
 print(user_2.followers)
