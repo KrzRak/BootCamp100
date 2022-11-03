e11b65317d2ba9792a2fe94a744d1a4bf603e94b
KrzRak <krzysztofrak93@gmail.com>
2022-11-03 11:59:57 +0100

    Course no.237

--- 026_List_Comperhension/main.py
+++ 026_List_Comperhension/main.py
@@ -17,12 +17,38 @@
 # test = [1 for n in names if len(n) > 4]
 ####=====================================================###no.235
 
+# # List Comperhension structure
+# from operator import truediv
+
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# squared = [n**2 for n in numbers if n > 0]
+
+# print(squared)
+# true = 1
+
+####=====================================================###no.236
+
+# # List Comperhension structure
+# from operator import truediv
+
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# result = [n for n in numbers if n % 2 == 0]
+
+# true = 1
+
+####=====================================================###no.237
+
 # List Comperhension structure
 from operator import truediv
 
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+with open("026_List_Comperhension/file1.txt") as file1:
+    file_1_data = file1.readlines()
 
-squared = [n**2 for n in numbers if n > 0]
+with open("026_List_Comperhension/file2.txt") as file2:
+    file_2_data = file2.readlines()
 
-print(squared)
-true = 1
+result = [int(n) for n in file_1_data if n in file_2_data]
+
+print(result)
