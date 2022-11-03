2c839253ded2453ef4e23aa7d2f07f6ae364ad33
KrzRak <krzysztofrak93@gmail.com>
2022-11-03 11:28:46 +0100

    Course no. 235

--- 026_List_Comperhension/main.py
+++ 026_List_Comperhension/main.py
@@ -1,10 +1,19 @@
 # List Comperhension structure
 from operator import truediv
 
-
 new_list = [1, 2, 3]
 new_numbers = [n+1 for n in new_list]
 
-true = 5
-while true > 1:
-    x = 1
+name = "Angela"
+letters_list = [letter for letter in name]
+
+numbers = range(1,5)
+double = [n*2 for n in range(1,5)]
+
+names = ["Ales", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [n for n in names if len(n) < 5]
+uppernames = [n.upper() for n in names if len(n) > 4]
+
+test = [1 for n in names if len(n) > 4]
+
+true = 1
