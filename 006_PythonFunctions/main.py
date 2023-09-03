b1ac656300328128df5617b1d793b100675d8d3d
KrzRak <krzysztofrak93@gmail.com>
2023-09-03 11:31:05 +0200

    Course no.058-066

---
+++ 006_PythonFunctions/main.py
@@ -0,0 +1,32 @@
+# Functions v1.0
+# -----------------------------------------------------
+# Instructions
+# -----------------------------------------------------
+
+# -----------------------------------------------------
+
+def my_function():
+    #Do this
+    #Then do this
+    #Finally do this
+    print("Hello")
+
+# For loop
+list_of_items = [0, 1, 2]
+
+for item in list_of_items:
+    #Do something to each item
+    print("Hello")
+
+a = 0
+b = 10
+for number in range(a, b):
+    print(number)
+
+
+# While loop
+something_is_true = 0
+while something_is_true < 10:
+    #Do something repeatedly
+    print("Hello")
+    something_is_true += 5
